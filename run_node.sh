d019a8078086aa5e0e39736b42832cc56caf14c0
Dan <dan.cananau@gmail.com>
2021-04-14 11:40:08 +0300

    Test jenkins webhook and try nohup fix

--- run_node.sh
+++ run_node.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 ## This is mainly used while development.
-HTTP_ENDPOINT=8090 go run cmd/node/main.go &
+fuser -k 8090/tcp || true
+HTTP_ENDPOINT=8090 nohup go run cmd/node/main.go &
+exit
